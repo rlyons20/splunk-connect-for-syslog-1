e31a55d0c81e5be86c378963df57a57bf05809bb
Dhruv Patel <dhruv.patel2@CDSYS.LOCAL>
2021-02-02 15:31:29 +0530

    feat: ADDON-33205-added-security-events-for-test_checkpoint.py

--- tests/test_checkpoint.py
+++ tests/test_checkpoint.py
@@ -476,3 +476,94 @@ def test_checkpoint_audit_events_without_originsicname(
     record_property("message", message)
 
     assert resultCount == 1
+
+security_product_events_with_originsicname = [
+    'time={{ epoch }}|hostname={{ host }}|severity=High|confidence_level=High|product=Anti-Virus|action=Detect|ifdir=outbound|ifname=eth0|loguid={0x19532d95,0x979f5a67,0x2350f78b,0x40d23913}|origin=1.1.1.1|originsicname=cn\=cp_mgmt,o\=gw-02bd87..4zrt7d|sequencenum=1|time={{ epoch }}|dst=8.8.8.8|malware_action=DNS query for a site known to contain malware|malware_rule_id={A2B8ED86-C9D0-4B0E-9334-C3CFA223CFC2}|session_id={0x5fe63a69,0x4,0xdbc0fdd2,0xe4d0ac81}|src=5.5.5.5',
+    'time={{ epoch }}|hostname={{ host }}|product=Firewall|action=Accept|ifdir=inbound|loguid={0x3f58042c,0x1e77df46,0x1f1d6712,0x2e0a1aad}|origin=1.1.1.1|originsicname=cn\=cp_mgmt,o\=gw-02bd87..4zrt7d|time={{ epoch }}|dst=2.2.2.2|src=3.3.3.3',
+    'time={{ epoch }}|hostname={{ host }}|severity=Critical|product=URL Filtering|ifdir=outbound|loguid={0x5fe62536,0x1,0x6563a00a,0x3fffaefd}|origin=1.1.1.1|originsicname=cn\=cp_mgmt,o\=gw-02bd87..4zrt7d|sequencenum=3|time={{ epoch }}',
+    'time={{ epoch }}|hostname={{ host }}|severity=Informational|product=IPS|action=Drop|ifdir=inbound|ifname=eth0|loguid={0x5d31915e,0x6d33e67b,0xd6c2bda1,0x880b3b39}|origin=10.160.99.101|originsicname=cn\=cp_mgmt,o\=gw-02bd87..4zrt7d|time={{ epoch }}|attack=Streaming Engine: TCP SYN Modified Retransmission|dst=4.4.4.4',   
+    'time={{ epoch }}|hostname={{ host }}|product=HTTPS Inspection|ifdir=inbound|loguid={0x5ff9b62e,0x0,0x6563a00a,0x342067f2}|origin=1.1.1.1|originsicname=cn\=cp_mgmt,o\=gw-02bd87..4zrt7d|sequencenum=5|time={{ epoch }}|version=5|description=HTTPS Inspection Trusted CAs|status=Started|update_service=1|version=2.0',
+    'time={{ epoch }}|hostname={{ host }}|product=DLP|ifdir=inbound|loguid={0x5ff9b62e,0x0,0x6563a00a,0x342067f2}|origin=1.1.1.1|originsicname=cn\=cp_mgmt,o\=gw-02bd87..4zrt7d|sequencenum=5|time={{ epoch }}|version=5|description=HTTPS Inspection Trusted CAs|status=Started|update_service=1|version=2.0',
+    'time={{ epoch }}|hostname={{ host }}|product=MTA|ifdir=inbound|loguid={0x5ff9b62e,0x0,0x6563a00a,0x342067f2}|origin=1.1.1.1|originsicname=cn\=cp_mgmt,o\=gw-02bd87..4zrt7d|sequencenum=5|time={{ epoch }}|version=5|description=HTTPS Inspection Trusted CAs|status=Started|update_service=1|version=2.0',
+    'time={{ epoch }}|hostname={{ host }}|product=Anti Spam|ifdir=inbound|loguid={0x5ff9b62e,0x0,0x6563a00a,0x342067f2}|origin=1.1.1.1|originsicname=cn\=cp_mgmt,o\=gw-02bd87..4zrt7d|sequencenum=5|time={{ epoch }}|version=5|description=HTTPS Inspection Trusted CAs|status=Started|update_service=1|version=2.0',
+    'time={{ epoch }}|hostname={{ host }}|product=Threat Extraction|ifdir=inbound|loguid={0x5ff9b62e,0x0,0x6563a00a,0x342067f2}|origin=1.1.1.1|originsicname=cn\=cp_mgmt,o\=gw-02bd87..4zrt7d|sequencenum=5|time={{ epoch }}|version=5|description=HTTPS Inspection Trusted CAs|status=Started|update_service=1|version=2.0',
+    'time={{ epoch }}|hostname={{ host }}|product=Anti-Ransomware|ifdir=inbound|loguid={0x5ff9b62e,0x0,0x6563a00a,0x342067f2}|origin=1.1.1.1|originsicname=cn\=cp_mgmt,o\=gw-02bd87..4zrt7d|sequencenum=5|time={{ epoch }}|version=5|description=HTTPS Inspection Trusted CAs|status=Started|update_service=1|version=2.0',
+    'time={{ epoch }}|hostname={{ host }}|product=Anti-Exploit|ifdir=inbound|loguid={0x5ff9b62e,0x0,0x6563a00a,0x342067f2}|origin=1.1.1.1|originsicname=cn\=cp_mgmt,o\=gw-02bd87..4zrt7d|sequencenum=5|time={{ epoch }}|version=5|description=HTTPS Inspection Trusted CAs|status=Started|update_service=1|version=2.0',
+    'time={{ epoch }}|hostname={{ host }}|product=OS Exploit|ifdir=inbound|loguid={0x5ff9b62e,0x0,0x6563a00a,0x342067f2}|origin=1.1.1.1|originsicname=cn\=cp_mgmt,o\=gw-02bd87..4zrt7d|sequencenum=5|time={{ epoch }}|version=5|description=HTTPS Inspection Trusted CAs|status=Started|update_service=1|version=2.0',
+    'time={{ epoch }}|hostname={{ host }}|product=Application|ifdir=inbound|loguid={0x5ff9b62e,0x0,0x6563a00a,0x342067f2}|origin=1.1.1.1|originsicname=cn\=cp_mgmt,o\=gw-02bd87..4zrt7d|sequencenum=5|time={{ epoch }}|version=5|description=HTTPS Inspection Trusted CAs|status=Started|update_service=1|version=2.0',
+];
+
+security_product_events_without_originsicname = [
+    'time={{ epoch }}|hostname={{ host }}|severity=Critical|product=IPS|ifdir=outbound|loguid={0x5fe54ecd,0x0,0x6563a00a,0x3fffaefd}|origin=1.1.1.1|sequencenum=1|time={{ epoch }}',
+    'time={{ epoch }}|hostname={{ host }}|severity=Low|product=Anti-Malware|ifdir=inbound|loguid={0x60069d02,0x2e,0xe03ea00a,0x23654691}|origin=1.1.1.1|sequencenum=1|version=1|action_details=Started|scanned_drives=C:\|sig_ver=202012122231|src=2.2.2.2|src_machine_name=C7553927437|src_user_name=Administrator',
+    'time={{ epoch }}|hostname={{ host }}|severity=Low|product=Endpoint Compliance|ifdir=inbound|loguid={0x60069d03,0x0,0xe03ea00a,0x23654691}|origin=1.1.1.1|policy_guid={5E122911-49AE-40ED-A91B-0B56576E4549}|policy_name=default_compliance_policy|policy_type=60|product_family=Endpoint|src=2.2.2.2|src_machine_name=C7553927437|src_user_name=Administrator|user_name= |user_sid=S-1-5-21-1704411108-3626445783-306313190-500',
+    'time={{ epoch }}|hostname={{ host }}|severity=Low|product=Media Encryption & Port Protection|action=Allow|ifdir=inbound|loguid={0x60069d02,0x35,0xe03ea00a,0x23654691}|origin=1.1.1.1|media_class_id={4d36e965-e325-11ce-bfc1-08002be10318}|product_family=Endpoint|reading_data_access=Allow|src=2.2.2.2|src_machine_name=C7553927437|src_user_name=Administrator|user_name= |writing_data_access=Allow',
+    'time={{ epoch }}|hostname={{ host }}|severity=Low|confidence_level=High|product=Threat Emulation|action=Prevent|ifdir=inbound|loguid={0x6002cac9,0x5,0xe03ea00a,0x2a66d4c7}|origin=1.1.1.1|description=Endpoint File Reputation prevented malicious file (21011822364438_inj.exe)|detected_on= |event_type=TE Event|file_name=21011822364438_inj.exe|local_time={{ epoch }}|log_id=4000|policy_number=1|product_family=Endpoint|protection_name=Gen.Rep.exe|protection_type=File Reputation|resource=C:\KB4\Rassim\DataDir\Tests\\12\\21011822364438_inj.exe|src=2.2.2.2|src_machine_name=C7553927437|src_user_name=Administrator',
+    'time={{ epoch }}|hostname={{ host }}|severity=Critical|confidence_level=Medium|product=Forensics|action=Prevent|ifdir=inbound|loguid={0x6002bd5b,0x0,0xe03ea00a,0x2a66d4c7}|origin=1.1.1.1|sequencenum=1|version=1|additional_info= |attack_status=Dormant|policy_name=Default Forensics settings|product_family=Endpoint|protection_name=Gen.Rep.exe|protection_type=File Reputation|src=2.2.2.2|src_machine_name=C7553927437|src_user_name=Administrator|suspicious_events=Remote Logon Internal: ; Valid Accounts: ; |user_name= ',
+    'time={{ epoch }}|hostname={{ host }}|severity=Critical|confidence_level=High|product=Anti-Bot|action=Prevent|ifdir=inbound|loguid={0x5ffd6981,0x16,0xe03ea00a,0x2a66d4c7}|origin=1.1.1.1|event_type=Anti Bot Event|policy_name=Default Anti-Bot settings|policy_number=1|process_username=Administrator|product_family=Endpoint|protection_name=Phishing.TC.aqbrwu|protection_type=URL Reputation|proxy_src_ip=3.3.3.3|src=4.4.4.4|src_machine_name=C7553927437|src_user_name=Administrator',
+    'time={{ epoch }}|hostname={{ host }}|severity=Informational|confidence_level=High|product=Zero Phishing|action=Detect|ifdir=inbound|loguid={0x5ffc205f,0xb,0xe03ea00a,0x2a66d4c7}|origin=1.1.1.1|event_type=Phishing Event|product_family=Endpoint|protection_name=gen.ba.phishing|protection_type=Phishing Prevention|src=2.2.2.2|src_machine_name=C7553927437|src_user_name=Administrator',
+];
+
+@pytest.mark.parametrize("event", security_product_events_with_originsicname)
+def test_checkpoint_security_events_with_originsicname(
+    record_property, setup_wordlist, setup_splunk, setup_sc4s, event
+):
+    host = "{}-{}".format(random.choice(setup_wordlist), random.choice(setup_wordlist))
+
+    dt = datetime.datetime.now()
+    iso, bsd, time, date, tzoffset, tzname, epoch = time_operations(dt)
+
+    # Tune time functions for Checkpoint
+    epoch = epoch[:-7]
+
+    mt = env.from_string(event + "\n")
+    message = mt.render(host=host, epoch=epoch)
+
+    sendsingle(message, setup_sc4s[0], setup_sc4s[1][514])
+
+    st = env.from_string(
+        'search _time={{ epoch }} index=netops host="cp_mgmt" sourcetype="cp_log" source="cp:security" _raw="{{ message }}"'
+    )
+    search = st.render(
+        epoch=epoch, bsd=bsd, host=host, date=date, time=time, tzoffset=tzoffset, message=message.replace('\\','\\\\').replace('|', '\|')
+    )
+
+    resultCount, eventCount = splunk_single(setup_splunk, search)
+
+    record_property("host", host)
+    record_property("resultCount", resultCount)
+    record_property("message", message)
+
+    assert resultCount == 1
+
+@pytest.mark.parametrize("event", security_product_events_without_originsicname)
+def test_checkpoint_security_events_without_originsicname(
+    record_property, setup_wordlist, setup_splunk, setup_sc4s, event
+):
+    host = "{}-{}".format(random.choice(setup_wordlist), random.choice(setup_wordlist))
+
+    dt = datetime.datetime.now()
+    iso, bsd, time, date, tzoffset, tzname, epoch = time_operations(dt)
+
+    # Tune time functions for Checkpoint
+    epoch = epoch[:-7]
+
+    mt = env.from_string(event + "\n")
+    message = mt.render(host=host,epoch=epoch)
+
+    sendsingle(message, setup_sc4s[0], setup_sc4s[1][514])
+
+    st = env.from_string(
+        'search _time={{ epoch }} index=netops host="{{ host }}" sourcetype="cp_log" source="cp:security" _raw="{{ message }}"'
+    )
+    search = st.render(
+        epoch=epoch, bsd=bsd, host=host, date=date, time=time, tzoffset=tzoffset, message=message.replace('\\','\\\\').replace('|', '\|') 
+    )
+
+    resultCount, eventCount = splunk_single(setup_splunk, search)
+
+    record_property("host", host)
+    record_property("resultCount", resultCount)
+    record_property("message", message)
+
+    assert resultCount == 1
